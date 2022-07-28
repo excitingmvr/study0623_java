16b863432a51571ae54aebaebcef053239335023
excitingmvr <excitingmvr@gmail.com>
2022-07-28 16:38:14 +0900

    first

---
+++ src/com/mycompany/stduy/ch02/Hello.java
@@ -0,0 +1,28 @@
+package com.mycompany.stduy.ch02;
+
+public class Hello {
+	
+	public static void main (String[] args) {
+
+//		데이터타입 변수명 = 값;
+//		데이터타입 변수명
+
+//		정수 : int
+//		실수 : double
+//		문자 : char
+//		블리언 : boolean : true, false 둘중 한개만 값으로 가질수 있음
+//		문자열 : String
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		String name = "장원영";
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은: " + name);
+
+
+		
+	}
+}
