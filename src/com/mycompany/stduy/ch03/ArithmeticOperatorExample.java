0e4776e2f5ea474b069068860e9fb7409bf852bb
excitingmvr <excitingmvr@gmail.com>
2022-08-03 17:30:46 +0900

    asdfasdf

--- src/com/mycompany/stduy/ch03/ArithmeticOperatorExample.java
+++ src/com/mycompany/stduy/ch03/ArithmeticOperatorExample.java
@@ -38,8 +38,29 @@ public class ArithmeticOperatorExample {
 		System.out.println("result5=" + result7);
 		
 		
+		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
+		
+		avg("홍길동", 60, 70, 85, 90, 45);
+		avg("박말순", 50, 75, 70, 60, 70);
+		avg("고장난", 55, 60, 64, 58, 90);
+		avg("도룡용", 80, 70, 63, 88, 78);
 		
 		
+	}
+	
+	public static void sum(int a, int b, int c) {
+		
+		System.out.println(a + " + " + b + " + " + c + " = " + (a+b+c) );
+		
+	}
+	
+	public static void avg (String name, int english, int math, int physics, int music, int athletic) {
+		
+		System.out.println(name + " 의 평균은: " + (double) (english + math + physics + music + athletic)/5 );
 	}
 
 }
