3a46b604e296d23c61517a5d0328c0407c516dcd
excitingmvr <excitingmvr@gmail.com>
2022-08-23 17:20:46 +0900

    asdf

--- src/com/mycompany/stduy/ch06/Korean.java
+++ src/com/mycompany/stduy/ch06/Korean.java
@@ -1,6 +1,23 @@
 package com.mycompany.stduy.ch06;
 
 public class Korean {
+	
+	String name;
+	String ssn;
+	int age;
+	
+	Korean(){
+	
+	}
+	
+	Korean(String name, String ssn){
+		this.name = name;
+		this.ssn = ssn;
+	}
 
+	Korean(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
 	
 }
--- src/com/mycompany/stduy/ch06/KoreanExample.java
+++ src/com/mycompany/stduy/ch06/KoreanExample.java
@@ -6,9 +6,9 @@ public class KoreanExample {
 		System.out.println("k1.name : " + k1.name);
 		System.out.println("k1.ssn : " + k1.ssn);
 		
-		Korean k2 = new Korean("장원영", "930525-0654321");
+		Korean k2 = new Korean("장원영", 45);
 		System.out.println("k2.name : " + k2.name);
-		System.out.println("k2.ssn : " + k2.ssn);
+		System.out.println("k2.age : " + k2.age);
 	}
 
 }
